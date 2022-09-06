0acebe258c99c8d63afba911ad3eace959896fe8
Kuma4K <55283091+Kuma4K@users.noreply.github.com>
2022-09-06 17:38:22 +0200

    Refactored main1.py and added utils1 to seperate all functions

---
+++ A1/main.py
@@ -0,0 +1,20 @@
+import utils1
+
+def main():
+    input = utils1.read_input_from_file()
+    """   Loop on all inputs and print Output"""
+    for j in range(len(input)):
+        priceSum = 0
+        taxSum = 0
+        print("Output " + str(j+1) + ":")
+        for i in range(len(input[j])):
+            item_list = utils1.getStringAttr(input[j][i])
+            tax_list = utils1.calc_total(item_list)
+            priceSum += tax_list[0]
+            taxSum += tax_list[1]
+            print(str(item_list[0]) + " " + item_list[1] + ": " + "{:.2f}".format(tax_list[0]))
+        print("> " + "Sales Taxes: " + "{:.2f}".format(taxSum) + "\n> Total: " + "{:.2f}".format(priceSum))
+
+
+if __name__ == '__main__':
+    main()
---
+++ A1/utils1.py
@@ -0,0 +1,59 @@
+import math
+
+
+def read_input_from_file():
+    f = open('input1.txt')
+    content = f.read()
+    f.close()
+
+    content_list = content.splitlines()
+    content_list.pop(0)
+    content_list.pop(0)
+
+    input1 = [content_list[0], content_list[1], content_list[2]]
+    input2 = [content_list[4], content_list[5]]
+    input3 = [content_list[7], content_list[8], content_list[9], content_list[10]]
+    input = [input1, input2, input3]
+    return input
+
+
+# Get 1 line of input and Returns Count of Items, item name, item price, if imported and if taxes
+def getStringAttr(text):
+    imported = False
+    taxes = True
+    text_helper = text.split()
+    last = len(text_helper) - 1
+    count = text_helper[0]
+    price = text_helper[last]
+    if text.find("imported") > 0:
+        imported = True
+    if text.find("book") > 0 or text.find("chocolate") > 0 or text.find("pills") > 0:
+        taxes = False
+    text_helper.remove("at")
+    text_helper.remove(count)
+    text_helper.remove(price)
+    item = ' '.join(text_helper)
+    return count, item, price, imported, taxes
+
+
+# Get price and boolean for Imported and taxes; Returns total item price and total item taxes
+def calc_total(r):
+    tax = 0
+    importTax = 0
+    price = float(r[2])
+    imported = r[3]
+    taxes = r[4]
+    if taxes:
+        tax = price * 0.1
+        tax = myRound(tax)
+    if imported:
+        importTax = price * 0.05
+        importTax = myRound(importTax)
+    sumTaxes = float("{:.2f}".format(tax + importTax))
+    total = float("{:.2f}".format(price + sumTaxes))
+    return total, sumTaxes
+
+
+# round on x.x5 or x.x0
+def myRound(x, base=0.05):
+    return float("{:.2f}".format(float(base * math.ceil(x / base))))
